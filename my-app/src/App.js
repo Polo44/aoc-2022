fd3b7542c8f950ea090e0c25428a84c6d292a1ab
pminot <paul.minot@aquasys.fr>
2022-12-05 09:45:28 +0100

    answer 5

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import Answer1 from './components/answer1/Answer1';
 import Answer2 from './components/answer2/Answer2';
 import Answer3 from './components/answer3/Answer3';
 import Answer4 from './components/answer4/Answer4';
+import Answer5 from './components/answer5/Answer5';
 
 const App = ({
 }) => {
@@ -13,6 +14,7 @@ const App = ({
             <Answer2 />
             <Answer3 />
             <Answer4 />
+            <Answer5 />
         </div>
     )
 }
---
+++ my-app/src/components/answer5/Answer5.js
@@ -0,0 +1,56 @@
+import { dropRight, flatten, takeRight, zip } from "lodash";
+import React, { useEffect, useState } from "react";
+import raw from './input.txt'
+
+const Answer5 = ({
+}) => {
+    const [data, setData] = useState([])
+    const [moves, setMoves] = useState([])
+
+    const transpose = (matrix) => {
+        return zip(...matrix)
+    }
+
+    useEffect(() => {
+        fetch(raw)
+            .then(r => r.text())
+            .then(text => {
+                const parsed = text.replaceAll('\r', '').split('\n')
+                const matrix = transpose(parsed.slice(0, 8).reverse().map((d, i) => d.split('').map((d2, i2) => i2 % 4 === 1 ? d2 : '').filter(d => d !== '')))
+                setData(matrix.map(d => d.filter(d2 => !!d2 && d2 !== ' ')))
+                setMoves(parsed.slice(10).map(d => d.replace('move ', '').replace(' to ', ' from ').split(' from ').map(d2 => parseInt(d2))))
+            });
+    }, [])
+
+    useEffect(() => {
+        let dataTemp = data
+        moves.forEach(([nbElement, startIndex, endIndex]) => {
+            const newColumn = [...dataTemp[endIndex - 1], ...takeRight(dataTemp[startIndex - 1], nbElement)/*.reverse() */] // for ex 1 uncomment
+            dataTemp = dataTemp.map((d, i) => {
+                if (i === startIndex - 1) {
+                    return dropRight(d, nbElement)
+                } else if (i === endIndex - 1) {
+                    return newColumn
+                } else {
+                    return d
+                }
+            })
+        setData(dataTemp)
+        })
+    }, [moves])
+
+    console.log({data, moves})
+
+    return (
+        <div className="App">
+            <div>
+                {flatten(data.map(d => takeRight(d)))}
+            </div>
+            {/* <div>
+                {result2}
+            </div> */}
+        </div>
+    );
+}
+
+export default Answer5;
