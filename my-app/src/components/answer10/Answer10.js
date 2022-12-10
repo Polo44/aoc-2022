90a344d7274ba9392c5dcb7cfa980baf0753181e
Polo44 <paul.minot.37@gmail.com>
2022-12-10 11:16:58 +0000

    exo 2 part 2

--- my-app/src/components/answer10/Answer10.js
+++ my-app/src/components/answer10/Answer10.js
@@ -14,28 +14,36 @@ const Answer10 = () => {
 
     const cycleExtract = [20, 60, 100, 140, 180, 220]
     
-    const checkResult = (acc) => cycleExtract.includes(acc.cycle) ? { ...acc, count: acc.count + (acc.cycle * acc.cycleCounter)} : acc
+    const draw = (acc) => {
+        const actualPos = acc.picture.replace('\n', '').length % 40
+        const newCarac = actualPos >= acc.cycleCounter - 1 && actualPos <= acc.cycleCounter + 1 ? '#' : '.'
+        return {
+            ...acc,
+            picture: acc.picture + newCarac + (acc.cycle % 40 === 0 ? '\n' : '')
+        }
+    } 
+    
+    const checkResult = (acc) => {
+        const newAcc = cycleExtract.includes(acc.cycle) ? { ...acc, count: acc.count + (acc.cycle * acc.cycleCounter)} : acc
+        return draw(newAcc)
+    }
     const noop = (acc) => checkResult({ ...acc, cycle: acc.cycle + 1})
     const addAcc = (acc, value) => {
         const newAcc = noop(acc)
         return checkResult({ ...newAcc, cycle: newAcc.cycle + 1, cycleCounter: newAcc.cycleCounter + value })
     }
     
-    const result1 = data.reduce((acc, value) => 
+    const result = data.reduce((acc, value) => 
         value[0] === 'noop' ? noop(acc) : addAcc(acc, parseInt(value[1]))
-    , { count: 0, cycle: 0, cycleCounter: 1})
-
-    const result2 = data.reduce((acc, value) => {
-        if (cycleExtract.includes(acc.cycle)) {
-          console.log(acc)
-        }
-        return value[0] === 'noop' ? noop(acc) : addAcc(acc, parseInt(value[1]))
-    }, { count: 0, cycle: 0, cycleCounter: 1})
+    , { count: 0, cycle: 0, cycleCounter: 1, picture : ''})
 
     return (
         <div className="App">
             <div>
-                {result1.count}
+                {result.count}
+            </div>
+            <div style={{ whiteSpace: 'pre-line'}}> 
+                {result.picture}
             </div>
         </div>
     )
