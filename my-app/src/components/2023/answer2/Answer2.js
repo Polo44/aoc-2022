ba3b532159b6bd4c5de568bcdd219bb6bef9bcf5
pminot <paul.minot@aquasys.fr>
2023-12-02 16:24:52 +0100

    day 2 clean

--- my-app/src/components/2023/answer2/Answer2.js
+++ my-app/src/components/2023/answer2/Answer2.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from 'react'
 import raw from './input.txt'
 import { max, keys } from 'lodash'
 
-const Answer2 = ({
-}) => {
+const Answer2 = () => {
     const [data, setData] = useState([])
 
     useEffect(() => {
@@ -20,28 +19,27 @@ const Answer2 = ({
         blue: 14
     }
 
-    const answer1 = data.reduce((acc, line) => {
+    const { answer1, answer2 } = data.reduce((acc, line) => {
         const [stringId, game] = line.replaceAll('\r', '').split(':')
         const setup = game.replaceAll(';', ',').split(',').reduce((accGame, cubes) => {
             const [value, cube] = cubes.replace(' ', '').split(' ')
-            return (rules[cube] < parseInt(value))
-        }, false)
+            return {
+                ...accGame,
+                [cube]: max([accGame[cube], parseInt(value)]),
+                condition: accGame.condition && (rules[cube] >= parseInt(value)),
+            }
+        }, { red: 0, green: 0, blue: 0, condition: true })
 
-        return setup ? acc : acc + parseInt(stringId.replaceAll('Game ', ''))
-    }, 0)
-
-    const answer2 = data.reduce((acc, line) => {
-        const [stringId, game] = line.replaceAll('\r', '').split(':')
-        const setup = game.replaceAll(';', ',').split(',').reduce((accGame, cubes) => {
-            const [value, cube] = cubes.replace(' ', '').split(' ')
-            return { ...accGame, [cube]: max([accGame[cube], parseInt(value)])}
-        }, { red: 0, green: 0, blue: 0 })
-
-        return acc + (keys(setup).reduce((acc, key) => acc * setup[key], 1))
-    }, 0)
+        return {
+            ...acc,
+            answer1: acc.answer1 + (setup.condition ? parseInt(stringId.replaceAll('Game ', '')) : 0),
+            answer2: acc.answer2 + (keys(setup).reduce((accFact, key) => key !== 'condition' ? accFact * setup[key] : accFact, 1)),
+        }
+    }, { answer1: 0, answer2: 0 })
 
     return (
         <div className="App">
+            Day 2 :
             <div>
                 {answer1}
             </div>
