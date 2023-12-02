2d3d935fe799ec9a0146e7f6f34733459001ebf8
pminot <paul.minot@aquasys.fr>
2023-12-02 16:25:19 +0100

    clean app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,13 @@
 import './App.css';
 import React from 'react'
-// import Answer1 from './components/2022/answer1/Answer1';
-// import Answer2 from './components/2022/answer2/Answer2';
-// import Answer3 from './components/2022/answer3/Answer3';
-// import Answer4 from './components/2022/answer4/Answer4';
-// import Answer5 from './components/2022/answer5/Answer5';
-// import Answer6 from './components/2022/answer6/Answer6';
-// import Answer8 from './components/2022/answer8/Answer8';
-// import Answer10 from './components/2022/answer10/Answer10';
-// import Answer11 from './components/answer11/Answer11';
-// import Answer12 from './components/2022/answer12/Answer12';
 import Answer1 from './components/2023/answer1/Answer1';
 import Answer2 from './components/2023/answer2/Answer2';
 
-const App = ({
-}) => {
+const App = () => {
     return (
         <div className="App">
             <Answer1 />
             <Answer2 />
-            {/* <Answer2 />
-            <Answer3 />
-            <Answer4 />
-            <Answer5 />
-            <Answer6 />
-            <Answer8 />
-            <Answer10 /> */}
-            {/* <Answer11 /> */}
-            {/* <Answer12 /> */}
         </div>
     )
 }
